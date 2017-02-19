8820bf2a6679e21e6279d54cb749cf40c8262de5
William Miao <wimiao@microsoft.com>
2017-02-19 01:00:30 -0800

    first commit

---
+++ gitbat.bat
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+IF "%~1"=="" (
+		ECHO first parameter is mandatory
+		EXIT /B 1	
+	    )
+IF "%2"=="" SET man2=master
+IF NOT "%2"=="" SET man2=%2
+
+SET man1=%~1
+
+ECHO commit message = ^"%man1%^"
+ECHO branch = %man2%
+
+REM ...do stuff here...
+git add --all
+git commit -m %man1%
+git push -u origin %man2%
+
+:theend
